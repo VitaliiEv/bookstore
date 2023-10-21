7096b1caafd9e39723a29ead906f79b14ff92c8b
Vitalii Solomonov <solomonov.v@gmail.com>
2023-10-21 18:52:41 +0300

    init root project

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "vitaliiev"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
